c867f383a4d46a7b63415a08f9970ceef420365b
juampiludu <juanzakka@gmail.com>
2020-06-24 10:44:31 -0300

    clase13

--- clase13.sql
+++ clase13.sql
@@ -13,6 +13,17 @@ VALUES(1, 'Yo', 'Soy', 'yosoy@gmail.com', (select address_id
 											order by address_id desc
 											limit 1), 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
+select * from customer inner join address using (address_id) inner join city using (city_id) inner join country using(country_id) where first_name='Yo'
+										
+-- forma bien hecha
+
+INSERT INTO customer
+(store_id, first_name, last_name, email, address_id, active, create_date, last_update)
+SELECT 1, 'ALICE', 'SMITH', 'test@domain.com', MAX(address_id), 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP FROM address
+		INNER JOIN city USING (city_id)
+		INNER JOIN country USING (country_id)
+		WHERE country = 'United States';
+
 -- Add a rental
 -- 	Make easy to select any film title. I.e. I should be able to put 'film tile' in the where, and not the id.
 -- 	Do not check if the film is already rented, just use any from the inventory, e.g. the one with highest id.
@@ -50,6 +61,19 @@ UPDATE film set
 	release_year = 2004
 where rating = "R";
 
+-- -------------------------------------------------
+
+UPDATE film 
+	SET release_year = (CASE
+		WHEN rating = 'PG' THEN 2001
+		WHEN rating = 'G' THEN 2002
+		WHEN rating = 'NC-17' THEN 2003
+		WHEN rating = 'PG-13' THEN 2004
+		WHEN rating = 'R' THEN 2005
+		ELSE release_year
+	END)
+WHERE rating IN ('PG', 'G', 'NC-17', 'PG-13', 'R');
+
 -- Return a film
 -- 	Write the necessary statements and queries for the following steps.
 -- 	Find a film that was not yet returned. And use that rental id. Pick the latest that was rented for example.
@@ -61,10 +85,10 @@ inner join inventory using(film_id)
 left join rental using (inventory_id)
 where return_date is null
 and rental_date in (select rental_date from rental order by rental_date desc)
-limit 1)
+limit 1);
 
 UPDATE sakila.rental
-SET return_date=current_timestamp , last_update=CURRENT_TIMESTAMP
+SET return_date=date_add(current_timestamp, interval 7 day) , last_update=CURRENT_TIMESTAMP
 WHERE inventory_id=@inven_id;
 
 -- Try to delete a film
